e2db71457f2513794ef11b3d2836604485c817ec
roshan1975 <shahinurislam1975@gmail.com>
2026-09-17 19:16:23 +0600

    create a cheak balace method

--- index.py
+++ index.py
@@ -1,27 +1,29 @@
-# class BankAccount:
+class BankAccount:
 
-#     def __init__(self, name, balance):
-#         self.name = name
-#         self.__balance = balance
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
 
-# # Deposit Money
-#     def deposit(self, amount):
+# Deposit Money
+    def deposit(self, amount):
 
-#         if amount > 0:
-#             self.__balance += amount
-#             print(f"Deposited: ৳{amount}")
-#         else:
-#             print("Invalid amount!")
+        if amount > 0:
+            self.__balance += amount
+            print(f"Deposited: ৳{amount}")
+        else:
+            print("Invalid amount!")
 
-#     # Withdraw Money
-#     def withdraw(self, amount):
+    # Withdraw Money
+    def withdraw(self, amount):
 
-#         if amount > 0 and amount <= self.__balance:
-#             self.__balance -= amount
-#             print(f"Withdrawn: ৳ {amount}")
-#         else:
-#             print("Invalid amount or insufficient balance!")
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            print(f"Withdrawn: ৳ {amount}")
+        else:
+            print("Invalid amount or insufficient balance!")
 
-    
+    def check_balance(self):
+        print(f"Account Holder: {self.name}")
+        print(f"Current Balance: ৳{self.__balance}")
 # -------------------                ------------------
 
