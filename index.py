03e64dacd6b6d164756ef59502816fc850724634
roshan1975 <shahinurislam1975@gmail.com>
2026-09-25 18:34:32 +0600

    Update index.py
    add a new feature

--- index.py
+++ index.py
@@ -44,6 +44,12 @@ name = input("Enter your name: ")
 initial_balance = float(input("Enter initial balance: "))
 account = BankAccount(name, initial_balance)
 
+# Create second account
+receiver_name = input("Enter receiver name: ")
+receiver_balance = float(input("Enter receiver initial balance: "))
+
+receiver = BankAccount(receiver_name, receiver_balance)
+
 while True:
 
     print("\n================================")
@@ -53,29 +59,33 @@ while True:
     print("1. Deposit")
     print("2. Withdraw")
     print("3. Check Balance")
-    print("4. Exit")
+    print("4. Transfer Money")
+    print("5. Receiver Balance")
+    print("6. Exit")
 
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        
         amount = float(input("Enter deposit amount: "))
         account.deposit(amount)
-
-    elif choice == "2":
-
-        amount = float(
-            input("Enter withdrawal amount: "))
         
+    elif choice == "2":
+        amount = float(input("Enter withdrawal amount: "))
         account.withdraw(amount)
 
     elif choice == "3":
         account.check_balance()
 
     elif choice == "4":
-        
-        print("Thank you for using My Bank! 👋")
-        break
+        amount = float(input("Enter transfer amount: "))
+        account.transfer(receiver, amount)
 
+    elif choice == "5":
+        receiver.check_balance()
+
+    elif choice == "6":
+        print("\nThank you for using My Bank! 👋")
+        break
+        
     else:
         print("Invalid choice!")
